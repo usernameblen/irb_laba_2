d656df365d615dd4920ae734bc9b2b7cab7bc688
ксюююшкааааа <fedulovaksenia26@gmail.com>
2025-01-22 20:37:45 +0300

    Update 2_1.c

--- 2_1.c
+++ 2_1.c
@@ -2,61 +2,82 @@
 #include<stdlib.h>
 #pragma warning(disable: 4996)
 
-char* function(int a, int r) {
-	int mask = (1 << r) - 1;
-	char *str = (char *)malloc(100);
-	if (str == NULL) {
-		return NULL;
+int plus_some(int number, int carry) { //carry - слагаемое
+	while (carry != 0) {
+		// сложение без переноса
+		int sum = number ^ carry;
+		// если первое выражение = 0 => перенос не нужен
+		carry = (number & carry) << 1;
+		// обновляем number 
+		number = sum;
 	}
+	return number;
+}
+
+int minus_some(int number, int b) {
+	return plus_some(number, plus_some(~b, 1));
+}
+
+int function(int a, int r, char *str, size_t size) {
+	if (r < 1 || r > 5) {
+		return 1;
+	}
+
+	int mask = minus_some((1 << r), 1);
 	int index = 0;
 
 	//переводим число из одной СС в другую
-	while (a != 0 && index < 99) {
+	while (a != 0 && index < minus_some(size, 1)) {
 		int ost = a & mask;
 		if (ost < 10) {
 			str[index++] = '0' | ost;
 		}
 		else {
-			str[index++] = 'A' | (ost - 10);
+			str[index++] = 'A' | minus_some(ost, 10);
 		}
 		a = a >> r;
 	}
 	str[index] = '\0';
 
-	//освобождаем память которая не понадобилась
-	
-	str = (char*)realloc(str, index+1);
-	if (str == NULL) {
-		return NULL;
-	}
 	//переворачиваем строку
 	int half_len = index >> 1;
-	
-	for(int i = 0; i < half_len; i++){
+
+	for (int i = 0; i < half_len; i = plus_some(i, 1)) {
+		int q = minus_some(minus_some(index, i), 1);
 		char temp = str[i];
-		str[i] = str[index - i - 1];
-		str[index - i - 1] = temp;
+		str[i] = str[q];
+		str[q] = temp;
 	}
 
-	return  str;
+	return 0;
 }
 
 
 int main() {
 	int a, r;
 	printf("Enter number a:");
-	scanf("%d", &a);
+
+	if (scanf("%d", &a) != 1) { //функция scanf возвращает кол-во успешно считанных элементов
+		printf("\ninvalid number a\n");
+		return 1;
+	}
 
 	printf("\nEnter the value of r (the number a will move to SS 2^r): ");
-	scanf("%d", &r);
 
-	char* result = function(a, r);
-	if (result == NULL) {
-		printf("Erorr whit memory.\n");
+	if (scanf("%d", &r) != 1) {
+		printf("\ninvalid number r\n");
+		return 1;
 	}
-	else {
+
+	char result[40];
+
+	switch (function(a, r, &result, 40)) {
+	case 0:
 		printf("\nNumber %d in SS 2^%d = %s", a, r, result);
+		break;
+	case 1:
+		printf("Error! r can be from 1 to 5.\n");
+		break;
 	}
-	free(result);
 	return 0;
 }
